75138d57036b0bff2f0fb8ccd3f91c822ae65bf8
Vaibhavi296 <74854198+Vaibhavi296@users.noreply.github.com>
2021-05-04 18:04:46 +0400

    Add files via upload

---
+++ Desktop Notification System.py
@@ -0,0 +1,17 @@
+import time
+
+
+
+from plyer import notification
+
+if __name__ == '__main__':
+    # while True:
+        notification.notify(
+        title="Please Drink Water Now",
+        message="You have to drink one glass of water",
+        # app_icon="C:\Users\vaibh\Downloads\Iconsmind-Outline-Glass-Water.ico",
+        app_icon="Iconsmind-Outline-Glass-Water.ico",
+        timeout=10
+        )
+    # time.sleep(60*60)
+
